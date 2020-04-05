40083a41b9ac4e0932eab8615ad1c4cb46c38a31
ahsanhabib98 <ahredoan@gmail.com>
2020-04-05 17:51:31 +0600

    added update and delete operation

--- skillbook_app/urls.py
+++ skillbook_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import (
     PostCreatView,
+    PostUpdateView,
+    PostDeleteView,
     PostCategoryView,
     add_to_like,
     remove_from_like
@@ -10,6 +12,8 @@ app_name = 'posts'
 
 urlpatterns = [
     path('post-create/', PostCreatView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/category/', PostCategoryView.as_view(), name='post_category'),
     path('<int:pk>/add-to-like/', add_to_like, name='add_to_like'),
     path('<int:pk>/remove-from-like/', remove_from_like, name='remove_from_like')
--- skillbook_app/views.py
+++ skillbook_app/views.py
@@ -3,7 +3,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import get_object_or_404, redirect
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView
+)
 
 from skillbook_app.forms import PostForm
 from skillbook_app.models import Post, PostCategory
@@ -20,6 +26,30 @@ class PostCreatView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
         profile = Profile.objects.get(user=self.request.user)
         form.instance.author = profile
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_update.html'
+    success_message = 'Successfully updated your post.'
+
+    def form_valid(self, form):
+        profile = Profile.objects.get(user=self.request.user)
+        form.instance.author = profile
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_id'] = self.get_object().category.pk
+        return context
+
+
+class PostDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Post
+    template_name = 'posts/post_confirm_delete.html'
+    success_message = 'Successfully deleted your post.'
+    success_url = reverse_lazy('home')
 
 
 class PostCategoryView(DetailView):
